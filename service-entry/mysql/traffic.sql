724f5bde51a4f33d00b30c6081ebb92926fb710d
Kwong-Hung YIP <kwonghung.yip@gmail.com>
2021-01-29 17:11:32 +0800

    Create traffic.sql

---
+++ service-entry/mysql/traffic.sql
@@ -0,0 +1,18 @@
+drop procedure if exists dummy;
+
+delimiter $$
+
+create procedure dummy()
+begin
+  declare cnt int default 1;
+
+  while cnt <= 10 do
+    select current_time() from dual;
+    do sleep(1);
+    set cnt = cnt + 1;
+  end while;
+end $$
+
+delimiter ;
+
+call dummy();
