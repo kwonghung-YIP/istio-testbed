3ee643fcdbae351e6ba302de9155bb205f6a487d
Kwong-Hung YIP <kwonghung.yip@gmail.com>
2021-02-01 15:45:09 +0800

    Update traffic.sql

--- service-entry/mysql/traffic.sql
+++ service-entry/mysql/traffic.sql
@@ -7,7 +7,8 @@ begin
   declare cnt int default 1;
 
   while cnt <= 10 do
-    select current_time(), user(), database() from dual;
+    select current_time(), user(), database();
+    show variables like "version";
     do sleep(1);
     set cnt = cnt + 1;
   end while;
