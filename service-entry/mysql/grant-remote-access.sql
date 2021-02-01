0ee7820ad9f71ad3cbf4cdcbafc0de5484b04d31
Kwong-Hung YIP <kwonghung.yip@gmail.com>
2021-02-01 13:54:06 +0800

    Update grant-remote-access.sql

--- service-entry/mysql/grant-remote-access.sql
+++ service-entry/mysql/grant-remote-access.sql
@@ -1,2 +1,5 @@
+--mount the following sql under "/docker-entrypoint-initdb.d" directory
+--the mysql server container will run scripts in above directory while DB init. 
+--
 grant all privileges on *.* to john@'%' identified by 'passw0rd' with grant option;
 flush privileges;
