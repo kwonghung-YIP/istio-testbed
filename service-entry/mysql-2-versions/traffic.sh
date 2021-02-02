75bedf9b9c607d78aaee9a65543c0264eab973ee
Kwong-Hung YIP <kwonghung.yip@gmail.com>
2021-02-02 11:26:22 +0800

    Update traffic.sh

--- service-entry/mysql-2-versions/traffic.sh
+++ service-entry/mysql-2-versions/traffic.sh
@@ -4,6 +4,6 @@ while :
 do
    mysql \
      -h 192.168.28.134 -P 3306 -D test \
-     -u john -p'passw0rd' -e 'source /root/traffic.sql'
+     -u john -p'passw0rd' -e 'source /root/mysql/traffic.sql'
   sleep 1
 done
