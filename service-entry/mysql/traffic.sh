1c262297e4c1a839de5c3b0a5ffab3aa22f8e445
Kwong-Hung YIP <kwonghung.yip@gmail.com>
2021-02-02 09:51:12 +0800

    Update traffic.sh

--- service-entry/mysql/traffic.sh
+++ service-entry/mysql/traffic.sh
@@ -3,7 +3,7 @@
 while :
 do
    mysql \
-     -h 192.168.28.130 -P 3306 -D test \
+     -h 192.168.28.134 -P 3306 -D test \
      -u john -p'passw0rd' -e 'source /root/traffic2.sql'
   sleep 1
 done
